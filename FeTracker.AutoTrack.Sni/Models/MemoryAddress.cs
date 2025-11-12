a6c45e1bcfa622732b17ae5eeffb401a961080f1
Antidale <antidale@tellah.life>
2025-11-12 11:20:16 -0800

    Update to net 10, no multi-target stuff

--- FeTracker.AutoTrack.Sni/Models/MemoryAddress.cs
+++ FeTracker.AutoTrack.Sni/Models/MemoryAddress.cs
@@ -3,6 +3,9 @@
     public readonly struct MemoryAddress
     {
         public uint Address { get; init; }
+        /// <summary>
+        /// A zero size indicates that ?? what was past me thinking just stopping typing there
+        /// </summary>
         public uint Size { get; init; }
 
     }
