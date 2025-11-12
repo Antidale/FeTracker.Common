bc44ce20221d99fa06a78e3a3f0af21d27a4fa1b
Antidale <antidale@tellah.life>
2025-11-12 11:28:37 -0800

    re-add pushign the common and autotrack packages

--- PushPackage.ps1
+++ PushPackage.ps1
@@ -2,6 +2,6 @@ param(
     [string] $version = "1.0.0"
 )
 
-# dotnet nuget push ~/LocalNuGet/FeTracker.Common.$version.nupkg --api-key $Env:FE_TrackerNugetApiKey
-# dotnet nuget push ~/LocalNuGet/FeTracker.AutoTrack.Sni.$version.nupkg --api-key $Env:FE_TrackerNugetApiKey
+dotnet nuget push ~/LocalNuGet/FeTracker.Common.$version.nupkg --api-key $Env:FE_TrackerNugetApiKey
+dotnet nuget push ~/LocalNuGet/FeTracker.AutoTrack.Sni.$version.nupkg --api-key $Env:FE_TrackerNugetApiKey
 dotnet nuget push ~/LocalNuGet/FeTracker.Common.RazorComponents.$version.nupkg --api-key $Env:FE_TrackerNugetApiKey
